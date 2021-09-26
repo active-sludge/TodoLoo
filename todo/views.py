0a4a00500d6111eaa922d90f044bfd212114150f
active-sludge <hcdayan@hotmail.com>
2021-09-26 14:15:00 +0300

    New repo. Add Dockerfile. requirements.txt.

---
+++ todo/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout,authenticate
+from .forms import TodoForm
+from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
+
+def sign_up_user(request):
+    if request.method == "GET":
+        return render(request, 'todo/signup.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password= request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signup.html',
+                              {'form': UserCreationForm(), 'error': 'That user name has already has been taken. Please choose a new username.'})
+        else:
+            return render(request, 'todo/signup.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+
+
+@login_required
+def current_todos(request):
+    todos = Todo.objects.filter(user=request.user, date_completed__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
+
+
+@login_required
+def log_out_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def log_in_user(request):
+    if request.method == "GET":
+        return render(request, 'todo/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render(request, 'todo/login.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match.'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+@login_required
+def create_todo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+            form  = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html',
+                          {'form': TodoForm(), 'error': 'Bad data entry. Try again.'})
+
+
+@login_required
+def todo_detail(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/todo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/todo.html', {'todo': todo, 'form': form, 'error':'Bad info.'})
+
+
+@login_required
+def complete_todo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.date_completed = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+@login_required
+def delete_todo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+
+@login_required
+def completed_todos(request):
+    todos = Todo.objects.filter(user=request.user, date_completed__isnull=False).order_by('-date_completed')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
---
+++ todoloo/urls.py
@@ -0,0 +1,37 @@
+"""todoloo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.sign_up_user, name='signupuser'),
+    path('logout/', views.log_out_user, name='logoutuser'),
+    path('login/', views.log_in_user, name='loginuser'),
+
+    # Todos
+    path('', views.home, name='home'),
+    path('create/', views.create_todo, name='createtodo'),
+    path('currenttodos/', views.current_todos, name='currenttodos'),
+    path('completed/', views.completed_todos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.todo_detail, name='todo'),
+    path('todo/<int:todo_pk>/complete', views.complete_todo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.delete_todo, name='deletetodo'),
+]
