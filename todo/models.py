a563ed0e505425f7715aa0ea314972e65230adbb
active-sludge <hcdayan@hotmail.com>
2021-09-27 20:53:53 +0300

    Add Article model

--- todo/models.py
+++ todo/models.py
@@ -12,3 +12,12 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Article(models.Model):
+    article_id = models.CharField(max_length=20)
+    article_title = models.CharField(max_length=200)
+    article_abstract = models.TextField(blank=True)
+    author_list = models.TextField(blank=True)
+    keyword_list = models.TextField(blank=True)
+    pub_date = models.CharField(max_length=100)
